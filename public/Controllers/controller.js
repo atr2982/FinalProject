a09b958b67e9d6ff1d67ceb223e692b8f9aacead
atr2982 <atr2982@fullsail.edu>
2015-12-04 20:59:54 -0500

    made data visual and added crud

--- public/Controllers/controller.js
+++ public/Controllers/controller.js
@@ -1,25 +1,72 @@
-/**
- * Created by patrickhalton on 12/1/15.
- */
+var myApp = angular.module('myApp', []);
 
-var myApp = angular.module('myApp',[]);
+myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
+    console.log("controller");
 
-myApp.controller('AppCtrl',['$scope','$http', function($scope,$http){
-        console.log("controller");
+    
+    
+    var refresh = function() {
+        $http.get('/beerdata').success(function (response) {
+            console.log("i got the data i requested");
+            $scope.userList = response;
+            $scope.user = "";
+        });
+    };
+    
+    refresh();
+    
+    
+    
 
-
+    //SIGN-UP    SIGN-UP
     //*data flow 2* controller receives data from view when button is clicked below
-    $scope.signUp = function(){
+    $scope.signUp = function () {
 
-            if($scope.user.password != $scope.user.confirm){
-                console.log("u suck")
-            }else{
-                console.log($scope.user);
-                $http.post('/beerdata',$scope.user).success(function(response){//*data flow 3* $http(ajax call) to server.js file
-                    console.log(response);
-                })
-
-                 }
-           };
-
+        if ($scope.user.password != $scope.user.confirm) {
+            console.log("passwords don't match");
+            return;
+        }
+            
+        console.log($scope.user);
+        $http.post('/beerdata', $scope.user).success(function (response) { //*data flow 3* $http(ajax call) to server.js file
+            console.log(response);
+            refresh();
+        });
+    };
+    
+    
+    $scope.remove = function(id) {
+        console.log(id);  
+        $http.delete('/beerdata/' + id).success(function(response) {
+            refresh();
+        });
+    };
+    
+    
+    $scope.edit = function(id) {
+        console.log(id);  
+        $http.get('/beerdata/' + id).success(function(response) {
+            console.log(response);
+            $scope.user = response;
+        });
+    };
+    
+    
+    $scope.update = function() {
+        console.log($scope.user._id);
+        $http.put('/beerdata/' + $scope.user._id, $scope.user).success(function(response) {
+            refresh();  
+        })
+    };
+    
+    
+    $scope.clear = function() {
+        console.log("deselect");
+        $scope.user = "";   
+    }
+    
+    
+    
+    
+    
 }]);
--- server.js
+++ server.js
@@ -1,35 +1,75 @@
-/**
- * Created by patrickhalton on 12/2/15.
- */
 var express = require('express'),
-        app = express(),
+    app = express(),
     mongojs = require('mongojs'),
-         db = mongojs('beerdata',['userlist']),
- bodyParser = require('body-parser');
+    db = mongojs('beerdata', ['userlist']),
+    bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + "/public"));//tells the server where to look for static files in the "public" folder
+
+app.use(express.static(__dirname + "/public")); //tells the server where to look for static files in the "public" folder
 app.use(bodyParser.json());
 
-app.get('/beerdata', function(req,res){
-        console.log("GET request");
 
-db.userlist.find(function(err,docs){
 
-    console.log("i received");
-    res.json(docs);
+//GET USER DATA
+//GET USER DATA
+app.get('/beerdata', function (req, res) {
+    console.log("GET request");
 
-     });
-
-});
-
-app.post('/beerdata', function(req,res){//*data flow 4* app.get written to match $http.get in controller.js so the server can receive the data
-    console.log(req.body);
-    //req is the data going into the front end
-    db.userlist.insert(req.body,function(err,doc){//doc is the object that is
-       res.json(doc);//*data flow 5* res is the data coming back from the server/ res gets transferred to controller.js -->
+    db.userlist.find(function (err, docs) {
+        console.log("i got the data");
+        res.json(docs);
     });
 });
 
 
+//REGISTER
+//REGISTER
+app.post('/beerdata', function (req, res) { //*data flow 4* app.get written to match $http.get in controller.js so the server can receive the data
+    console.log(req.body);
+    //req is the data going into the front end
+    db.userlist.insert(req.body, function (err, doc) { //doc is the object that is
+        res.json(doc); //*data flow 5* res is the data coming back from the server/ res gets transferred to controller.js -->
+    });
+});
+
+
+//DELETE
+//DELETE
+app.delete('/beerdata/:id', function(req, res) {
+    var id = req.params.id;
+    console.log(id);
+    db.userlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc) {
+        res.json(doc);
+    })
+});
+
+
+//EDIT
+//EDIT
+app.get('/beerdata/:id', function(req, res) {
+    var id = req.params.id;
+    console.log(id);
+    db.userlist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc) {
+        console.log(doc);
+        res.json(doc);
+    });
+});
+
+
+//update after edit
+app.put('/beerdata/:id', function(req, res) {
+    var id = req.params.id;
+    console.log(req.body.name);
+    db.userlist.findAndModify({query: {_id: mongojs.ObjectId(id)},
+        update: {$set: {username: req.body.username, email: req.body.email}},
+        new: true}, function(err, doc) {
+        res.json(doc);
+        
+    });
+});
+
+
+
+
 app.listen(3000);
-console.log("running on 3000");
+console.log("running on 3000"); 
