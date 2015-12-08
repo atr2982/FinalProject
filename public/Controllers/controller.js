bbaad17bd38be4f82de148e02b55e35ae25ac696
atr2982 <atr2982@fullsail.edu>
2015-12-07 19:22:41 -0500

    added api search funtionality

--- public/Controllers/controller.js
+++ public/Controllers/controller.js
@@ -1,10 +1,10 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
-    console.log("controller");
 
     
-    
+    //GET USER DATA
+    //GET USER DATA
     var refresh = function() {
         $http.get('/beerdata').success(function (response) {
             console.log("i got the data i requested");
@@ -16,11 +16,10 @@ myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
     refresh();
     
     
-    
-
-    //SIGN-UP    SIGN-UP
+    //SIGN-UP
+    //SIGN-UP    
     //*data flow 2* controller receives data from view when button is clicked below
-    $scope.signUp = function () {
+    $scope.signUp = function() {
 
         if ($scope.user.password != $scope.user.confirm) {
             console.log("passwords don't match");
@@ -34,7 +33,8 @@ myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
         });
     };
     
-    
+    //REMOVE
+    //REMOVE  
     $scope.remove = function(id) {
         console.log(id);  
         $http.delete('/beerdata/' + id).success(function(response) {
@@ -42,7 +42,8 @@ myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
         });
     };
     
-    
+    //EDIT
+    //EDIT    
     $scope.edit = function(id) {
         console.log(id);  
         $http.get('/beerdata/' + id).success(function(response) {
@@ -51,7 +52,8 @@ myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
         });
     };
     
-    
+    //UPDATE
+    //UPDATE     
     $scope.update = function() {
         console.log($scope.user._id);
         $http.put('/beerdata/' + $scope.user._id, $scope.user).success(function(response) {
@@ -59,11 +61,26 @@ myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
         })
     };
     
-    
+    //CLEAR
+    //CLEAR     
     $scope.clear = function() {
         console.log("deselect");
         $scope.user = "";   
     }
+    
+    
+    //SEARCH
+    //SEARCH    
+    //*data flow 2* controller receives data from view when button is clicked below
+    $scope.search = function() {
+            
+        console.log($scope.search.term);
+            $http.get("https://api.untappd.com/v4/search/beer?q=" + $scope.search.term + "&limit=10&client_id=905F449B2E3DAB14D4138D35623F50858F2D105D&client_secret=B4DEB76167F86248BB68F5CDA7606A8EA2707752")
+            .success(function(response){
+                $scope.details = response;
+                console.log(response);
+            });
+    };
     
     
     
