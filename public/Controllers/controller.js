d15b5428664dcbe437742090be76721511dae1cb
Patrick Halton <phalton04@fullsail.edu>
2015-12-07 19:19:08 -0500

    routes set up, additional login functionality

--- public/Controllers/controller.js
+++ public/Controllers/controller.js
@@ -1,35 +1,32 @@
-/**
- * Created by patrickhalton on 12/1/15.
- */
-
-/**
- * Created by patrickhalton on 8/31/15.
- */
 
 angular.module('myApp', ['ngRoute']).config(["$routeProvider",function ($routeProvider) {
+
         $routeProvider
             .when('/', {
-                templateUrl: 'public/views/signUp.html',
+                templateUrl: '/views/signUp.html',
                 controller: 'AppCtrl'
+
+            }).when('/home', {
+                templateUrl: '/views/homepage.html',
+                controller: 'homepageCtrl'
 
             })
 
     }])
 
-    .controller('appCtrl',["$scope","$http","$location", function ($scope,$http,$location) {
+    .controller('AppCtrl',["$scope","$http","$location", function ($scope,$http,$location) {
+        $scope.signUp1 = false;
 
-        console.log("controller");
-
-        //*data flow 2* controller receives data from view when button is clicked below
         $scope.signUp = function(){
 
             if($scope.user.password != $scope.user.confirm){
-                console.log("u suck")
+                $location.path('/');
+                alert("passwords do not match");
             }else{
                 console.log($scope.user);
                 $http.post('/beerdata',$scope.user).success(function(response){//*data flow 3* $http(ajax call) to server.js file
                     console.log(response);
-                    $location.path('/homepage');
+                    $location.path('/home');
 
                 })
 
@@ -39,10 +36,13 @@ angular.module('myApp', ['ngRoute']).config(["$routeProvider",function ($routePr
 
     }])
 
+
+
     .controller('homepageCtrl',["$scope","$location", function ($scope,$location) {
 
-
-
+      $scope.logout = function(){
+          $location.path('/');
+      };
 
     }]);
 
--- server.js
+++ server.js
@@ -5,10 +5,23 @@ var express = require('express'),
         app = express(),
     mongojs = require('mongojs'),
          db = mongojs('beerdata',['userlist']),
- bodyParser = require('body-parser');
+ bodyParser = require('body-parser'),
+    passport       = require('passport'),
+    LocalStrategy  = require('passport-local').Strategy;
 
-
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        db.userlist.findOne({ username: username }, function (err, user) {
+            if (err) { return done(err); }
+            if (!user) { return done(null, false); }
+            if (!user.verifyPassword(password)) { return done(null, false); }
+            return done(null, user);
+        });
+    }
+));
 
 app.get('/beerdata', function(req,res){
         console.log("GET request");
@@ -31,9 +44,8 @@ app.post('/beerdata', function(req,res){//*data flow 4* app.get written to match
     });
 });
 
-
 app.get('*', function (req, res) {
-    res.sendFile('/public/index.html');
+    res.sendFile('/index.html');
 });
 app.listen(3000);
 console.log("running on 3000");
